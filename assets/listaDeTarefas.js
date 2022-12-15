ffaa1f90b625f22d81f142a6e29aece2ad9540ec
Glauco <glauco.rodrigo1@gmail.com>
2022-12-14 22:30:02 -0300

    concluindo ações do botão concluir

--- assets/listaDeTarefas.js
+++ assets/listaDeTarefas.js
@@ -12,6 +12,7 @@ const criarTarefa = (evento) => {
 
     tarefa.innerHTML = conteudo
 
+    tarefa.appendChild(BotaoConclui())
     lista.appendChild (tarefa)
     input.value = " "
 }
@@ -20,5 +21,22 @@ const novaTarefa = document.querySelector('[data-form-button]')
 
 novaTarefa.addEventListener('click',criarTarefa)
 
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement ('button')
+    botaoConclui.classList.add('check-button')
+    botaoConclui.innerText = 'Concluir'
+    botaoConclui.addEventListener('click',concluirTarefa)
+    return botaoConclui
 
+}
+
+
+const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target
+
+    const tarefaCompleta = botaoConclui.parentElement
+
+    tarefaCompleta.classList.toggle('done')
+
+}
 
